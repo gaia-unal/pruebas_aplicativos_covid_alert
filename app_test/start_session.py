a1e29496bc37e1f58fd0fc2c8298f0902a55e212
julian506 <jupachon@unal.edu.co>
2021-05-19 18:45:15 -0500

    Created an script that allows to test the start of session on the app. The tester must change the cellphone string variable value to the tester's cellphones number

---
+++ app_test/start_session.py
@@ -0,0 +1,36 @@
+import time
+
+from appium import webdriver
+
+
+def start_session_test(driver):
+    cellphone = "3146587759" #Change this string as you need
+
+    userCellphoneTextBox = driver.find_element_by_xpath(
+        "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText")
+    userCellphoneTextBox.send_keys(
+        cellphone)
+    time.sleep(2)
+    loginButton = driver.find_element_by_accessibility_id(
+        "Ingresar")
+    loginButton.click()
+    time.sleep(20) #in this time the tester has to write manually the code received in the Cellphone
+
+
+if __name__ == "__main__":
+    caps = {}
+    caps["platformName"] = "Android"
+    caps["platformVersion"] = "10"
+    caps["deviceName"] = "ginkgo"
+    caps["automationName"] = "UiAutomator1"
+    caps["appPackage"] = "com.example.aprendiendo"
+    caps["appActivity"] = ".MainActivity"
+    
+    driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", caps)
+    
+    #Sleep for 30 seconds because of the initial informative image of the app and to wait the app to ask location permissions and allow it
+    time.sleep(30)
+    allowLocationAccess = driver.find_element_by_id("com.android.permissioncontroller:id/permission_allow_foreground_only_button")
+    allowLocationAccess.click()
+
+    start_session_test(driver)
