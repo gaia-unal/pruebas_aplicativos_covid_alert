1035269d86aa754c437db8b22dae97879ad8f27c
julian506 <jupachon@unal.edu.co>
2021-05-20 10:24:14 -0500

    Created script test to invite contacts when the user is logged in as a registered user

---
+++ app_test/invite_contacts_registered.py
@@ -0,0 +1,27 @@
+import time
+
+from appium import webdriver
+
+from start_session import start_session_test
+from invite_contacts import invite_contacts_test
+
+def invite_contacts_registered_test(driver):
+    start_session_test(driver)
+    invite_contacts_test(driver)
+
+if __name__ == "__main__":
+    caps = {}
+    caps["platformName"] = "Android"
+    caps["platformVersion"] = "10"
+    caps["deviceName"] = "ginkgo"
+    caps["automationName"] = "UiAutomator1"
+    caps["appPackage"] = "com.example.aprendiendo"
+    caps["appActivity"] = ".MainActivity"
+    
+    driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", caps)
+    
+    #Sleep for 30 seconds because of the initial informative image of the app and to wait the app to ask location permissions and allow it
+    time.sleep(30)
+    allowLocationAccess = driver.find_element_by_id("com.android.permissioncontroller:id/permission_allow_foreground_only_button")
+    allowLocationAccess.click()
+    invite_contacts_registered_test(driver)
