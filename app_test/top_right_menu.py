19f9e264b018bc902f76d3e53d7b8e52ff61d659
julian506 <jupachon@unal.edu.co>
2021-05-21 17:53:12 -0500

    Automated in order, in disorder and random access test to the elements of the top right menu. Still waiting for the developer to release all the other options of the menu.

--- app_test/top_right_menu.py
+++ app_test/top_right_menu.py
@@ -1,39 +1,74 @@
 import time
+import random
 
 from appium import webdriver
 
-constant = "hola"
+# Every of these options allways returns to the main menu
+
+
+def mapping_top_right_menu_options(driver, options, topRightMenu):
+    topRightMenu.click()
+    time.sleep(1)
+    permissionControl = driver.find_element_by_accessibility_id(
+        "Control de permisos")
+    inviteYourContacts = driver.find_element_by_accessibility_id(
+        "Invita a tus contactos")
+    options.append(permissionControl)
+    options.append(inviteYourContacts)
+    driver.back()
+
+
+def navigation_in_order(driver, options, topRightMenu):
+    time.sleep(3)
+    topRightMenu.click()
+    for option in options:
+        option.click()
+        time.sleep(3)
+        driver.back()
+    driver.back()
+
+
+def navigation_in_disorder(driver, options, topRightMenu):
+    time.sleep(3)
+    topRightMenu.click()
+    options.reverse()
+    for option in options:
+        option.click()
+        time.sleep(3)
+        driver.back()
+    options.reverse()
+    driver.back()
+
+
+def random_navigation(driver, options, topRightMenu):
+    time.sleep(3)
+    topRightMenu.click()
+    for i in range(len(options)*2):
+        option = random.choice(options)
+        option.click()
+        time.sleep(3)
+        driver.back()
+    driver.back()
 
 
 def top_right_menu_test(driver):
-
+    time.sleep(3)
     topRightMenu = driver.find_element_by_xpath(
         "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]")
 
-    topRightMenu.click()
-
     options = []
+    mapping_top_right_menu_options(driver, options, topRightMenu)
 
-    permissionControl = driver.find_element_by_accessibility_id(
-        "Control de permisos")
-    options.append(permissionControl)
-    permissionControl.click()
-    time.sleep(5)
-    driver.back()
-
-    # From start screen to invite your contacts screen
-    inviteYourContacts = driver.find_element_by_accessibility_id(
-        "Invita a tus contactos")
-    inviteYourContacts.click()
-    options.append(inviteYourContacts)
-    allowContactsAccess = driver.find_element_by_id(
-        "com.android.permissioncontroller:id/permission_allow_button")
-    allowContactsAccess.click()
-    # 10 seconds to let the app load the contact list and to see if the app shows the list correctly
-    time.sleep(10)
-    driver.back()
+    print("En orden")
+    navigation_in_order(driver, options, topRightMenu)
+    print("En desorden")
+    navigation_in_disorder(driver, options, topRightMenu)
+    print("Aleatorio")
+    random_navigation(driver, options, topRightMenu)
 
     # Waiting for the rest of options to be released
+    time.sleep(3)
+    # driver.back() # Remind to always return to the main menu, not only to the top right menu view
 
 
 if __name__ == '__main__':
@@ -44,15 +79,12 @@ if __name__ == '__main__':
     caps["automationName"] = "UiAutomator1"
     caps["appPackage"] = "com.example.aprendiendo"
     caps["appActivity"] = ".MainActivity"
+    caps["autoGrantPermissions"] = "true"
 
     # Two options of the Top Right Menu working for the tests: "Control de Permisos" and "Invita a tus contactos". All the other options still doesn't work because the app developers haven't finished them and they are inaccessible.
     driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", caps)
     # Sleep for 30 seconds because of the initial informative image of the app and to wait the app to ask location permissions and allow it
-    time.sleep(30)
-
-    allowLocationAccess = driver.find_element_by_id(
-        "com.android.permissioncontroller:id/permission_allow_foreground_only_button")
-    allowLocationAccess.click()
+    time.sleep(15)
 
     # mapping
     continueWithoutRegister = driver.find_element_by_accessibility_id(
