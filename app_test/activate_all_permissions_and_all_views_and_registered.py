082ae33b9a77b7c5b98e5c6c48def7f44b72600c
PaulaTaborda7 <ptabordam@unal.edu.co>
2021-05-24 16:33:02 -0500

    Start session + control of permissions (activate all) + all views

---
+++ app_test/activate_all_permissions_and_all_views_and_registered.py
@@ -0,0 +1,47 @@
+import time
+
+from appium import webdriver
+
+from bottom_menu_navigation import bottom_menu_navigation_test
+from control_of_permissions_activate_all import \
+    control_of_permission_activate_all_test
+from start_session import start_session_test
+from top_right_menu import top_right_menu_test
+
+
+def activate_all_permissions_and_all_views_and_registered_test(driver):
+
+    start_session_test(driver)
+
+    # Activate all permissions (It is assumed that all permissions are turned off)
+    control_of_permission_activate_all_test(driver)
+    driver.back()
+    driver.back()
+
+    top_right_menu_test(driver)
+
+    time.sleep(5)
+
+    bottom_menu_navigation_test(driver)
+
+
+if __name__ == '__main__':
+
+    nameTelephone = "j4primelte"
+    versionAndroid = '9'
+
+    caps = {}
+    caps["platformName"] = "Android"
+    caps["platformVersion"] = versionAndroid
+    caps["deviceName"] = nameTelephone
+    caps["automationName"] = "UiAutomator1"
+    caps["appPackage"] = "com.example.aprendiendo"
+    caps["appActivity"] = ".MainActivity"
+    caps["autoGrantPermissions"] = 'true'
+
+    driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", caps)
+
+    # Sleep for 15 seconds because of the initial informative image of the app
+    time.sleep(15)
+
+    activate_all_permissions_and_all_views_and_registered_test(driver)
