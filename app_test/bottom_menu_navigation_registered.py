aac73710f765f521cf0d0b81f6d6a9dedcb69d95
PaulaTaborda7 <ptabordam@unal.edu.co>
2021-06-02 15:36:37 -0500

    The imports were organized and some unnecessary functions were deleted

--- app_test/bottom_menu_navigation_registered.py
+++ app_test/bottom_menu_navigation_registered.py
@@ -1,19 +1,8 @@
 import time
 
-import random
-
 from appium import webdriver
 
 from bottom_menu_navigation import bottom_menu_navigation_test
-
-from bottom_menu_navigation import run_in_ascending_order
-
-from bottom_menu_navigation import run_in_descending_order
-
-from bottom_menu_navigation import run_in_random_order
-
-from bottom_menu_navigation import run_in_desorder
-
 from start_session import start_session_test
 
 
@@ -25,21 +14,18 @@ def bottom_menu_navigation_registered_test(driver):
 
 if __name__ == '__main__':
 
-    nameTelephone = "j4primelte"
-    versionAndroid = '9'
-
     caps = {}
     caps["platformName"] = "Android"
-    caps["platformVersion"] = versionAndroid
-    caps["deviceName"] = nameTelephone
-    caps["automationName"]="UiAutomator1"
+    caps["platformVersion"] = '7'
+    caps["deviceName"] = 'joan'
+    caps["automationName"] = "UiAutomator1"
     caps["appPackage"] = "com.example.aprendiendo"
     caps["appActivity"] = ".MainActivity"
     caps["autoGrantPermissions"] = "true"
 
-    driver=webdriver.Remote("http://127.0.0.1:4723/wd/hub",caps)
+    driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", caps)
 
-    #Time needed to reach the "allow" button in the location permission window 
+    # Time needed to reach the "allow" button in the location permission window
     time.sleep(15)
 
     bottom_menu_navigation_registered_test(driver)
